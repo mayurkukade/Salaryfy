698775367fdcad9954b3cec3fafc535921fdb038
mayurkukade <mayurkukade@opticalarc.com>
2023-07-28 15:56:05 +0530

    added footer section

--- salaryfy/src/components/HomeComponent/Testimonals.tsx
+++ salaryfy/src/components/HomeComponent/Testimonals.tsx
@@ -1,10 +1,10 @@
 const Testimonals = () => {
   return (
-    <div>
-      <p className="text-center mt-[7.19rem] text-[3.9375rem] font-bold font-Lexend">
+    <div className="bg-darkGreen">
+      <p className="text-center mt-[7.19rem] text-[3.9375rem] font-bold font-Lexend ">
         Our <span className="text-yellow">speed</span> is your <span className="text-yellow">power</span>
       </p>
-      <p>testimonals</p>
+      <p className="text-white text-center">testimonals</p>
     </div>
   );
 };
--- salaryfy/src/pages/Home.tsx
+++ salaryfy/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import YellowWidth from "../components/HomeComponent/YellowWidth";
 import PlacementsPartners from "../components/HomeComponent/PlacementsPartners";
 import Testimonals from "../components/HomeComponent/Testimonals";
 import Universities from "../components/HomeComponent/Universities";
-
+import Footer from "../components/FooteComponent/Footer";
 const Home = () => {
   return (
     <>
@@ -17,6 +17,7 @@ const Home = () => {
         <PlacementsPartners/>
         <Testimonals/>
         <Universities/>
+        <Footer/>
       </div>
     </>
   );
