c9f0ca4c748ead43beb0729c53aa9d9d566201ae
devharp <hsingh.161297@gmail.com>
2023-07-28 13:15:54 +0530

    sandwich made another page

--- salaryfy/src/modules/questionnaire/questionnaire.module.tsx
+++ salaryfy/src/modules/questionnaire/questionnaire.module.tsx
@@ -69,43 +69,25 @@ export default function QuestionnaireModule0() {
 
             <SubStep status={SUB_STEP_STATUS.REMAINING} no={3} title='Confirming Interview Slot' />
           </div>
+          {/* <PersonalDetails/> */}
+          <ScreeningQuestions />
 
-          <div className='font-semibold text-[1.8em] text-[#5B5B5B]'>Fill the details below</div>
-
-          <div className='py-[3em] px-[10em]'>
-            <div className='bg-[#F3FAF9] rounded-md py-[3em] px-[7em]'>
-              <div className='flex mb-[4em]'>
-                <div className='flex flex-col flex-grow text-[#005F59] font-semibold text-[1.8em] pr-[1em]'>
-                  <div>Name</div>
-                  <div><input type="text" placeholder='Rahul' className='w-[100%] px-[0.5em] border border-[#005F59] border-solid rounded-md outline-none' /></div>
-                </div>
-                <div className='flex flex-col flex-grow text-[#005F59] font-semibold text-[1.8em] pl-[1em]'>
-                  <div>Name</div>
-                  <div><input type="text" placeholder='Rahul' className='w-[100%] px-[0.5em] border border-[#005F59] border-solid rounded-md outline-none' /></div>
-                </div>
-              </div>
-              <div className='flex'>
-                <div className='flex flex-col max-w-[50%] flex-grow text-[#005F59] font-semibold text-[1.8em] pr-[1em]'>
-                  <div>Phone number</div>
-                  <div className='flex'>
-                    <input type="text" placeholder='Rahul' className='mr-[1em] w-[100%] border border-[#005F59] border-solid rounded-md outline-none' />
-                    <div className='bg-[#005F59] text-[#FECD08] rounded-md font-medium p-[0.25em] text-[1em]'>Verify</div>
-                  </div>
-                </div>
-                <div className='flex items-center flex-grow text-[#005F59] font-semibold text-[1.8em] pl-[1em]'>
-                  <div className='flex flex-row items-center flex-grow h-[100%] px-[2em] py-[0.5em] justify-center rounded-[1em]' style={{ boxShadow: '0 0 5px rgb(0, 0, 0, 0.2)', backgroundColor: 'white' }}>
-                    <div className='mr-[0.5em]'>
-                      <svg width="39" height="39" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M14.625 27.625V17.875L11.375 21.125M14.625 17.875L17.875 21.125" stroke="#005F59" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M35.75 16.25V24.375C35.75 32.5 32.5 35.75 24.375 35.75H14.625C6.5 35.75 3.25 32.5 3.25 24.375V14.625C3.25 6.5 6.5 3.25 14.625 3.25H22.75" stroke="#005F59" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M35.75 16.25H29.25C24.375 16.25 22.75 14.625 22.75 9.75V3.25L35.75 16.25Z" stroke="#005F59" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
-                      </svg>
-                    </div>
-                    <div>Upload Resume</div>
-                  </div>
-                </div>
-
-              </div>
+          <div className='flex justify-center'>
+            <div className='flex items-center px-[1.5em] py-[0.5em] rounded-xl bg-[#B3B3B3] mx-[1em]'>
+              <span className='mr-[1em]'>
+                <svg width="35" height="25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1.12891 12.1289C0.647814 12.61 0.647813 13.39 1.12891 13.8711L8.96875 21.7109C9.44985 22.192 10.2299 22.192 10.7109 21.7109C11.192 21.2298 11.192 20.4498 10.7109 19.9688L3.74219 13L10.7109 6.03124C11.192 5.55015 11.192 4.77015 10.7109 4.28905C10.2299 3.80796 9.44985 3.80796 8.96876 4.28905L1.12891 12.1289ZM33 11.7681L2 11.7681L2 14.2319L33 14.2319L33 11.7681Z" fill="#5B5B5B" />
+                </svg>
+              </span>
+              <span className='text-[2em] text-[#5B5B5B] font-medium mr-[0.5em]'>Back</span>
+            </div>
+            <div className='flex items-center bg-[#FECD08] px-[1.5em] py-[0.5em] rounded-xl mx-[1em]'>
+              <span className='text-[2em] font-medium mr-[0.5em] text-[#005F59]'>Next</span>
+              <span className='' style={{ transform: 'scaleX(-1)' }}>
+                <svg width="35" height="25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M1.12891 12.1289C0.647814 12.61 0.647813 13.39 1.12891 13.8711L8.96875 21.7109C9.44985 22.192 10.2299 22.192 10.7109 21.7109C11.192 21.2298 11.192 20.4498 10.7109 19.9688L3.74219 13L10.7109 6.03124C11.192 5.55015 11.192 4.77015 10.7109 4.28905C10.2299 3.80796 9.44985 3.80796 8.96876 4.28905L1.12891 12.1289ZM33 11.7681L2 11.7681L2 14.2319L33 14.2319L33 11.7681Z" fill="#005F59" />
+                </svg>
+              </span>
             </div>
           </div>
         </div>
@@ -186,6 +168,184 @@ function Step({ className, title, no, active, progress }: { className?: string, 
 
     </div>
   );
+}
+
+function PersonalDetails() {
+  return (
+    <>
+      <div className='font-semibold text-[1.8em] text-[#5B5B5B]'>Fill the details below</div>
+      <div className='py-[3em] px-[10em]'>
+        <div className='bg-[#F3FAF9] rounded-md py-[3em] px-[7em]'>
+          <div className='flex mb-[4em]'>
+            <div className='flex flex-col flex-grow text-[#005F59] font-semibold text-[1.8em] pr-[1em]'>
+              <div>Name</div>
+              <div><input type="text" placeholder='Your Name' className='w-[100%] px-[0.5em] border border-[#005F59] border-solid rounded-md outline-none' /></div>
+            </div>
+            <div className='flex flex-col flex-grow text-[#005F59] font-semibold text-[1.8em] pl-[1em]'>
+              <div>Phone number</div>
+              <div><input type="text" placeholder='+91' className='w-[100%] px-[0.5em] border border-[#005F59] border-solid rounded-md outline-none' /></div>
+            </div>
+          </div>
+          <div className='flex mb-[4em]'>
+            <div className='flex flex-col max-w-[50%] flex-grow text-[#005F59] font-semibold text-[1.8em] pr-[1em]'>
+              <div>Email</div>
+              <div className='flex'>
+                <input type="text" placeholder='Email' className='mr-[1em] w-[100%] border border-[#005F59] border-solid rounded-md outline-none' />
+                <div className='bg-[#005F59] text-[#FECD08] rounded-md font-medium p-[0.25em] text-[1em]'>Verify</div>
+              </div>
+            </div>
+            <div className='flex items-center flex-grow text-[#005F59] font-semibold text-[1.8em] pl-[1em]'>
+              <div className='flex flex-row items-center flex-grow h-[100%] px-[2em] py-[0.5em] justify-center rounded-[1em]' style={{ boxShadow: '0 0 5px rgb(0, 0, 0, 0.2)', backgroundColor: 'white' }}>
+                <div className='mr-[0.5em]'>
+                  <svg width="39" height="39" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.625 27.625V17.875L11.375 21.125M14.625 17.875L17.875 21.125" stroke="#005F59" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M35.75 16.25V24.375C35.75 32.5 32.5 35.75 24.375 35.75H14.625C6.5 35.75 3.25 32.5 3.25 24.375V14.625C3.25 6.5 6.5 3.25 14.625 3.25H22.75" stroke="#005F59" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M35.75 16.25H29.25C24.375 16.25 22.75 14.625 22.75 9.75V3.25L35.75 16.25Z" stroke="#005F59" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+                  </svg>
+                </div>
+                <div>Upload Resume</div>
+              </div>
+            </div>
+          </div>
+          <div className='flex mb-[2em]'>
+            <div className='flex pb-[1em] flex-col max-w-[50%] flex-grow text-[#005F59] font-semibold text-[1.8em] pr-[1em]'>
+              <div className='mb-[0.25em]'>OTP</div>
+              <div className='flex justify-between'>
+                <div className='flex'>
+                  <input type="text" className='w-[2em] mr-[1em] border border-[#005F59] border-solid rounded-md outline-none' />
+                  <input type="text" className='w-[2em] mr-[1em] border border-[#005F59] border-solid rounded-md outline-none' />
+                  <input type="text" className='w-[2em] mr-[1em] border border-[#005F59] border-solid rounded-md outline-none' />
+                  <input type="text" className='w-[2em] border border-[#005F59] border-solid rounded-md outline-none' />
+                </div>
+                <div className='flex items-center'>
+                  <div className='mr-[0.25em]'>
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M6.07296 12.2577L6.00837 12.1477C5.02183 10.4668 2.40024 6.89977 2.37375 6.86395L2.33594 6.81257L3.2291 5.92988L6.05637 7.90408C7.83649 5.59411 9.49723 4.00751 10.5805 3.081C11.7655 2.0675 12.5369 1.60091 12.5447 1.59644L12.5622 1.58594H14.0773L13.9326 1.71482C10.2106 5.03 6.17634 12.0761 6.13616 12.1469L6.07296 12.2577Z" fill="#005F59" />
+                    </svg>
+                  </div>
+                  <div className='text-[0.75em]'>Verified</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>);
+}
+
+function ScreeningQuestions() {
+  return (
+    <>
+      <div className='font-semibold text-[1.8em] text-[#5B5B5B] mb-[1em]'>Fill the details below</div>
+      
+      <Question question='Do you currently live in the city for which you want to apply the job for?' />
+      <YesNoResponse className='text-[1.5em] ml-[1.5em] mb-[1em]' />
+      <QuestionSeparator className='mb-[2em]' />
+
+      <Question question='Do you currently live in the city for which you want to apply the job for?' />
+      <YesNoResponse className='text-[1.5em] ml-[1.5em] mb-[1em]' />
+      <QuestionSeparator className='mb-[2em]' />
+
+      <Question question='Do you currently live in the city for which you want to apply the job for?' />
+      <YesNoResponse className='text-[1.5em] ml-[1.5em] mb-[1em]' />
+      <QuestionSeparator className='mb-[2em]' />
+
+      <Question question='Do you currently live in the city for which you want to apply the job for?' />
+      <YesNoResponse className='text-[1.5em] ml-[1.5em] mb-[1em]' />
+      <QuestionSeparator className='mb-[2em]' />
+
+      <Question question='Do you currently live in the city for which you want to apply the job for?' />
+      <RatingResponse className='ml-[2em] mb-[1em]' />
+      <QuestionSeparator className='mb-[2em]' />
+
+      <Question question='Do you currently live in the city for which you want to apply the job for?' />
+      <RatingResponse className='ml-[2em] mb-[1em]' />
+      <QuestionSeparator className='mb-[2em]' />
+
+      <Question question='Do you currently live in the city for which you want to apply the job for?' />
+      <RatingResponse className='ml-[2em] mb-[1em]' />
+      <QuestionSeparator className='mb-[2em]' />
+
+      <Question question='Do you currently live in the city for which you want to apply the job for?' />
+      <RatingResponse className='ml-[2em] mb-[1em]' />
+      <QuestionSeparator className='mb-[2em]' />
+
+    </>);
+}
+
+function Question({ question }: { question: string }) {
+  return (
+    <div className='flex'>
+      <div className='mt-[0.25em] mr-[1em] h-[100%]'>
+        <svg width="15" height="18" viewBox="0 0 15 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect y="0.273438" width="12.2727" height="12.2727" rx="2" fill="#FECD08" />
+          <path d="M15 11.1815L4.77273 17.0862L4.77273 5.27674L15 11.1815Z" fill="#0E5F59" />
+        </svg>
+      </div>
+      <div className='text-[1.5em] font-medium'>
+        <div className='mb-[0.5em]'>{question}</div>
+      </div>
+    </div>
+  );
+}
+
+function YesNoResponse({ className }: { className?: string }) {
+  return (
+    <div className={'flex ' + className}>
+      <div className='flex items-center mr-[0.5em]'>
+        <span className='mr-[0.5em]'>
+          <AppRadioButton active={true} />
+        </span>
+        <span>Yes</span>
+      </div>
+      <div className='flex items-center'>
+        <span className='mr-[0.5em]'>
+          <AppRadioButton active={false} />
+        </span>
+        <span>No</span>
+      </div>
+    </div>
+  );
+}
+
+function RatingResponse({ className }: { className?: string }) {
+  function Star({ className, active }: { className?: string, active?: boolean }) {
+    return (<>
+      {active && <div className={className}><svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9.5 0L12.7945 4.96546L18.535 6.56434L14.8307 11.232L15.084 17.1857L9.5 15.105L3.91604 17.1857L4.16933 11.232L0.464963 6.56434L6.20546 4.96546L9.5 0Z" fill="#FECD08" />
+      </svg></div>}
+
+      {
+        !active && <div className={className}><svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9.5 0L12.7945 4.96546L18.535 6.56434L14.8307 11.232L15.084 17.1857L9.5 15.105L3.91604 17.1857L4.16933 11.232L0.464963 6.56434L6.20546 4.96546L9.5 0Z" fill="#D7E8F0" />
+        </svg></div>
+      }
+
+    </>);
+  }
+  return (
+    <div className={'flex ' + className}>
+        <Star className='mr-[1em]' active={true} />
+        <Star className='mr-[1em]' active={true} />
+        <Star className='mr-[1em]' active={true} />
+        <Star className='mr-[1em]' active={true} />
+        <Star className='' active={false} />
+    </div>
+  );
+}
+
+function QuestionSeparator({ className }: { className?: string }) {
+  return <div className={'bg-[#0E5F59] h-[1px] opacity-[0.2] w-[100%] ' + className}></div>
+}
+
+function AppRadioButton({ active }: { active?: boolean }) {
+  const radioButtonColor = active ? '#005F59' : '#5B5B5B';
+  return (
+    <svg className='cursor-pointer' viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em">
+      <circle cx="10" cy="10" r="9" stroke={radioButtonColor} strokeWidth="2" fill="none" />
+      {active && <circle cx="10" cy="10" r="5" fill="#005F59" opacity="1" />}
+    </svg>
+  )
 }
 
 export function QuestionnaireModule() {
