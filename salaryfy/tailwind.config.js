c7ae6b9d7d09c3d5a6df99ea61f319d57b33785c
optiarcAg <132900657+optiarcAg@users.noreply.github.com>
2023-07-28 10:36:50 +0530

    Feat - Data Added

--- salaryfy/tailwind.config.js
+++ salaryfy/tailwind.config.js
@@ -6,12 +6,28 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{
-        darkGreen : '#0E5F59',
-        white:"#FFFFFF",
-        yellow:"#FECD08",
-        greyBlue:"7B9E9C"
-      }
+      colors: {
+        primaryDarkGreen: "#005F59",
+        secondaryYellow1: "#FECD08",
+        secondaryYellow2: "FFCD05",
+        white: "#FFFFFF",
+        lightblue: " #E9FEFF",
+        grey: "#5B5B5B",
+        black: "#000000",
+       darkgrey:" #B3B3B3",
+      },
+      fontFamily: {
+        Lexend: [" Lexend,", "Gilroy-HeavyItalic;"],
+      },
+
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
     },
   
   },
