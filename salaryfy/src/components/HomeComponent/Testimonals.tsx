a2f1c699719d80a5fcf86afa8a6807b783ff7f21
mayurkukade <mayurkukade@opticalarc.com>
2023-07-28 15:13:15 +0530

    add university section

---
+++ salaryfy/src/components/HomeComponent/Testimonals.tsx
@@ -0,0 +1,12 @@
+const Testimonals = () => {
+  return (
+    <div>
+      <p className="text-center mt-[7.19rem] text-[3.9375rem] font-bold font-Lexend">
+        Our <span className="text-yellow">speed</span> is your <span className="text-yellow">power</span>
+      </p>
+      <p>testimonals</p>
+    </div>
+  );
+};
+
+export default Testimonals;
--- salaryfy/src/pages/Home.tsx
+++ salaryfy/src/pages/Home.tsx
@@ -3,6 +3,8 @@ import Navbar from "../components/NavComponent/Navbar";
 import OurStats from "../components/HomeComponent/OurStats";
 import YellowWidth from "../components/HomeComponent/YellowWidth";
 import PlacementsPartners from "../components/HomeComponent/PlacementsPartners";
+import Testimonals from "../components/HomeComponent/Testimonals";
+import Universities from "../components/HomeComponent/Universities";
 
 const Home = () => {
   return (
@@ -13,6 +15,8 @@ const Home = () => {
         <YellowWidth/>
         <OurStats/>
         <PlacementsPartners/>
+        <Testimonals/>
+        <Universities/>
       </div>
     </>
   );
