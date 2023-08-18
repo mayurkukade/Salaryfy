48d3f9259186ee64fdc1628fee4d54ed0a20f493
mayurOpticalarc.com <mayurkukade@opticalarc.com>
2023-08-18 11:05:56 +0530

    update

--- salaryfy - Copy/src/components/app-radio.button.component.tsx
+++
@@ -1,9 +0,0 @@
-export function AppRadioButton({ active }: { active?: boolean }) {
-  const radioButtonColor = active ? '#005F59' : '#5B5B5B';
-  return (
-    <svg className='cursor-pointer' viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em">
-      <circle cx="10" cy="10" r="9" stroke={radioButtonColor} strokeWidth="2" fill="none" />
-      {active && <circle cx="10" cy="10" r="5" fill="#005F59" opacity="1" />}
-    </svg>
-  )
-}
--- salaryfy - Copy/src/contants/app.contants.ts
+++
@@ -1,5 +0,0 @@
-const APP_CONSTANTS = {
-  SERVER_URL: 'http://localhost:8080'
-}
-
-export default APP_CONSTANTS;
