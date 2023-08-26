3a47a49307bd44bd7da0fb72004ad494ecccd64d
Aksh-007 <akshaybarapatre54@gmail.com>
2023-08-26 16:38:24 +0530

    Feat- Post slice and function created

---
+++ salaryfy/src/features/api-integration/screeningQuestion/screening.model.interface.ts
@@ -0,0 +1,9 @@
+export interface ScreeningData {
+    ans: string;
+    jobFairQ1Id: number;
+    jobId : string;
+    question: string;
+    questionType:string;
+    setNo:string;
+    userId:number
+  }
