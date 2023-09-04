bb3ce97a25944718c1b6972a48ef3b0ca5573419
Aksh-007 <akshaybarapatre54@gmail.com>
2023-09-04 10:49:13 +0530

    Fix - upload resume accept only pdf

--- salaryfy/src/modules/questionnaire/pages/personal-details.page.tsx
+++ salaryfy/src/modules/questionnaire/pages/personal-details.page.tsx
@@ -456,57 +456,57 @@ const ComfirmPassword: React.FC<ComfirmPassword> = (props) => {
 function UploadResumeComponent() {
   return (
     <div className="flex items-center flex-grow text-[#005F59] font-semibold text-[1.8em]">
-  <div
-    className="flex flex-row items-center flex-grow h-[100%] px-[2em] py-[0.5em] justify-center rounded-[1em]"
-    style={{
-      boxShadow: "0 0 5px rgb(0, 0, 0, 0.2)",
-      backgroundColor: "white",
-      cursor: "pointer", // Add cursor pointer for click interaction
-    }}
-    onClick={() => document.getElementById("resume-upload").click()}
-  >
-    <div className="mr-[0.5em]">
-      <label htmlFor="resume-upload">
-        <svg
-          width="39"
-          height="39"
-          viewBox="0 0 39 39"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M14.625 27.625V17.875L11.375 21.125M14.625 17.875L17.875 21.125"
-            stroke="#005F59"
-            strokeWidth="3"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M35.75 16.25V24.375C35.75 32.5 32.5 35.75 24.375 35.75H14.625C6.5 35.75 3.25 32.5 3.25 24.375V14.625C3.25 6.5 6.5 3.25 14.625 3.25H22.75"
-            stroke="#005F59"
-            strokeWidth="3"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M35.75 16.25H29.25C24.375 16.25 22.75 14.625 22.75 9.75V3.25L35.75 16.25Z"
-            stroke="#005F59"
-            strokeWidth="3"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </label>
-      <input
-        type="file"
-        id="resume-upload"
-        accept="application/pdf,application/vnd.ms-excel"
-        style={{ display: "none" }}
-      />
+    <div
+      className="flex flex-row items-center flex-grow h-[100%] px-[2em] py-[0.5em] justify-center rounded-[1em]"
+      style={{
+        boxShadow: "0 0 5px rgb(0, 0, 0, 0.2)",
+        backgroundColor: "white",
+        cursor: "pointer", // Add cursor pointer for click interaction
+      }}
+      onClick={() => document.getElementById("resume-upload").click()}
+    >
+      <div className="mr-[0.5em]">
+        <label htmlFor="resume-upload">
+          <svg
+            width="39"
+            height="39"
+            viewBox="0 0 39 39"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M14.625 27.625V17.875L11.375 21.125M14.625 17.875L17.875 21.125"
+              stroke="#005F59"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M35.75 16.25V24.375C35.75 32.5 32.5 35.75 24.375 35.75H14.625C6.5 35.75 3.25 32.5 3.25 24.375V14.625C3.25 6.5 6.5 3.25 14.625 3.25H22.75"
+              stroke="#005F59"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M35.75 16.25H29.25C24.375 16.25 22.75 14.625 22.75 9.75V3.25L35.75 16.25Z"
+              stroke="#005F59"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </label>
+        <input
+          type="file"
+          id="resume-upload"
+          accept="application/pdf,application/vnd.ms-excel"
+          style={{ display: "none" }}
+        />
+      </div>
+      <div>Upload Resume (PDF only)</div>
     </div>
-    <div>Upload Resume (PDF only)</div>
   </div>
-</div>
 
   );
 }
