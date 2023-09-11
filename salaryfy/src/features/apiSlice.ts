02744eb09a64dc2f70bcf04d6355a9a23f2f34c1
mayurOpticalarc.com <mayurkukade@opticalarc.com>
2023-09-11 19:59:04 +0530

    update

--- salaryfy/src/features/apiSlice.ts
+++ salaryfy/src/features/apiSlice.ts
@@ -5,5 +5,5 @@ import APP_CONSTANTS from "../contants/app.contants";
 export const  apiSlice = createApi({
     baseQuery:fetchBaseQuery({baseUrl: APP_CONSTANTS.SERVER_URL}),
     tagTypes:["User", "Jobs","interview-schedule", "jobs-filter", "job-id", "user-id", "user-resume", "user-files", "upcoming-interviews", "get-profile-qualiication", "jobs-recommended", "get-user-skills", "set-user-skills", "get-universities"],
-    endpoints: (builder) => ({}),
+    endpoints: () => ({}),
 })
