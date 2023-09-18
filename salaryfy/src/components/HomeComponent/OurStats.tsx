3a65dab2e70cd5268dbd8d46f4760514669b5772
mayurOpticalarc.com <mayurkukade@opticalarc.com>
2023-09-18 11:09:23 +0530

    update

--- salaryfy/src/components/HomeComponent/OurStats.tsx
+++ salaryfy/src/components/HomeComponent/OurStats.tsx
@@ -4,12 +4,12 @@ const OurStats = () => {
 
     <div className="bg-lightBlue">
       <div
-        className="container mx-auto h-[18rem] text-[4rem] flex flex-wrap items-center  justify-center md:justify-between px-4 md:px-11 text-center text-darkGreen md:h-[12rem] "
+        className="container mx-auto h-[18rem] text-[4rem] flex flex-wrap items-center  justify-center md:justify-between px-4 md:px-8 text-center text-darkGreen md:h-[10rem] "
         // style={{border:"2px solid black"}}
       >
         {/* Column 1 */}
         <div className="w-[50%] md:w-[25%] mb-8 ">
-          <p className="text-[3rem] font-semibold">
+          <p className="text-[3.3rem] font-semibold">
             <span className="text-yellow">Our</span>{" "}
             <span className="">Stats</span>{" "}
           </p>
