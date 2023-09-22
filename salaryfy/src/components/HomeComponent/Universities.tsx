0f3407c3f8ba34481dfc1f47a5a81c8dd48233e8
mayurOpticalarc.com <mayurkukade@opticalarc.com>
2023-09-22 10:12:38 +0530

    fix

--- salaryfy/src/components/HomeComponent/Universities.tsx
+++ salaryfy/src/components/HomeComponent/Universities.tsx
@@ -23,7 +23,7 @@ const Universities = () => {
       <div className="container mx-auto">
         <p className="text-center  mt-8 md:mt-16 mb-6 md:mb-15 text-2xl md:text-4xl font-semibold">
           <span className="text-darkGreen text-[3.9375rem]">Students from </span>
-          <span className="text-yellow text-[3.9375rem]">Universities</span>
+          <span className="text-yellow text-[3.9375rem]">University</span>
         </p>
         <div className="max-h-[400px] overflow-y-auto">
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8 mb-10 ">
