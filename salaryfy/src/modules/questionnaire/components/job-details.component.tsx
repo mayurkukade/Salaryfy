7da0e9c17dc49f8f9180381eedc3f476a8caadda
devharp <hsingh.161297@gmail.com>
2023-09-27 14:52:52 +0530

    fixed mobile responsive job logo

--- salaryfy/src/modules/questionnaire/components/job-details.component.tsx
+++ salaryfy/src/modules/questionnaire/components/job-details.component.tsx
@@ -105,7 +105,7 @@ export default function UserJobDetails() {
           <div className="text-[1.8em] font-semibold text-[#0E5F59] px-[2em] flex items-center">
             <div className="relative min-w-[5em] mt-[1em]">
               <div className="absolute h-[5em] w-[5em] p-[0.5em] flex items-center justify-center rounded-md bg-[#D7E8F0] -translate-y-full overflow-hidden">
-                <img src="/src/assets/images/lenskart-icon.png"></img>
+                <img src={content.logo}></img>
               </div>
               <div className="relative text-center">
                 {content?.companyName}
